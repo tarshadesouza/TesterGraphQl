a8c88ba639a63c3761138864b1d38cedcd2bb27f
Tarsha De Souza <tarsha.desouza@talentomobile.com>
2020-11-19 12:49:53 +0100

    initial commit

---
+++ TesterGraphQL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TesterGraphQL
+//
+//  Created by Tarsha De Souza on 19/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
